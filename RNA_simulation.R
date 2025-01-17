2063b46dee7a026b99b68ab226888e116998eaa4
shangsilin <sl5232@cumc.columbia.edu>
2025-01-17 12:46:54 -0500

    simulation completed

--- RNA_simulation.R
+++ RNA_simulation.R
@@ -5,6 +5,7 @@ library(writexl)
 library(ggrepel)
 library(plotly)
 library(randomForest)
+library(caret)
 
   #Load the dictionary, containing the most common 9 nucleotides(AUCG, their methylated form, and D)
 dictionary = read_xlsx("Data/dictionary.xlsx") %>% 
@@ -86,26 +87,51 @@ perfect_fragments = function(input_sequence, name_of_RNA) {
 ### THE CODE ABOVE IS TO SIMULATE RNA LADDER MASSES
 ### THE CODE BELOW IS TO TRAIN MODEL TO PREDICT RETERNTION TIME BASED ON MASS
 
-train_df = rbind(read_xlsx("Data/250115/Phe_1FA_10pmol_T01_250114_LSS.xlsx") %>% 
+train_df = read_xlsx("Data/phe_train_df.xlsx") %>% 
+  janitor::clean_names() %>% 
+  select(monoisotopic_mass, apex_rt, ladder_type) %>% 
   drop_na() %>% 
-  janitor::clean_names(),
-  
-  read_xlsx("Data/250115/Phe_1FA_15pmol_T01_250114_LSS.xlsx") %>% 
-    drop_na() %>% 
-    janitor::clean_names(),
+  filter(apex_rt <= 15) %>% 
+  mutate(ladder_type = as.factor(ladder_type))
 
-  read_xlsx("Data/250115/Phe_1FA_20pmol_T01_250114_LSS.xlsx") %>% 
-    drop_na() %>% 
-    janitor::clean_names(),
-
-  read_xlsx("Data/250115/Phe_50FA_15pmol_T01_250114_LSS.xlsx") %>% 
-    drop_na() %>% 
-    janitor::clean_names())
+# Add the rows to the train_data dataset
+train_df = rbind(train_df, data.frame(
+  monoisotopic_mass = c(500, 500, 1000, 1000, 1500, 1500),  
+  ladder_type = as.factor(c("5'", "3'", "5'", "3'", "5'", "3'")), 
+  apex_rt = c(0.4, 0.35, 0.5, 0.45, 0.6, 0.55)))
 
 # Fit a Random Forest model
-rf_model <- randomForest(apex_rt ~ monoisotopic_mass, data = train_df, ntree = 500)
+rf_model <- randomForest(
+  apex_rt ~ .,  # Specify the target variable and use all other columns as predictors
+  data = train_df,  # Training data
+  ntree = 500,        # Number of trees to grow
+  mtry = sqrt(ncol(train_df) - 1),  # Number of predictors sampled at each split
+  importance = TRUE   # Compute variable importance
+)
 
-# Predict on the training data
-train_df$predicted_apex_rt_rf <- predict(rf_model, train_df)
+tune_grid <- expand.grid(
+  mtry = c(1, sqrt(ncol(train_df) - 1), ncol(train_df) - 1)
+)
 
-# Visualize predictions
+# Train the random forest model
+set.seed(1)
+rf_model <- train(
+  apex_rt ~ ., 
+  data = train_df, 
+  method = "rf", 
+  trControl = trainControl(method = "cv", number = 5),
+  tuneGrid = tune_grid,
+  ntree = 500 # Set ntree separately
+)
+
+### THE CODE ABOVE IS FOR TRAINING THE MODEL
+### THE CODE BELOW IS FOR GENERATING THE COMPLETE DATASET FOR TESTING ALGORITHM
+
+generate_dataset = function(length, RNA_name, intensity_mean){
+  simulated_df = perfect_fragments(simulate_RNA(length), RNA_name)
+  simulated_df$apex_rt = predict(rf_model, simulated_df)
+  simulated_df$sum_intensity = pmax(1000, rnorm(nrow(simulated_df), mean = intensity_mean, sd = 20000)) 
+  simulated_df$sum_intensity[simulated_df$position == length] = runif(1, min = 1, max = 5) * max(simulated_df$sum_intensity)
+  simulated_df$relative_abundance = simulated_df$sum_intensity/max(simulated_df$sum_intensity) * 100
+  return(simulated_df)
+}
