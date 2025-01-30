36b7d953c81f72b730f23e3dd6dc675a9a257c16
shangsilin <sl5232@cumc.columbia.edu>
2025-01-30 12:07:36 -0500

    saving

--- figure5.R
+++ figure5.R
@@ -1,2 +1,15 @@
 library(tidyverse)
 library(dplyr)
+library(readxl)
+library(writexl)
+
+ppm = function(observed, theo){
+  if(abs((observed - theo) / theo * 10^6) > 10) {
+    return(FALSE)
+  } else {
+    return(TRUE)
+  }
+}
+
+dictionary = read_xlsx("Data/dictionary.xlsx")
+dictionary = dictionary[-c(5, 9),]
