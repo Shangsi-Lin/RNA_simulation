114e885b68e1081f072cfe8876c9beffafbf9bc0
shangsilin <sl5232@cumc.columbia.edu>
2025-02-10 12:34:24 -0500

    figure 5 completed

--- figure5.R
+++ figure5.R
@@ -10,3 +10,21 @@ library(ggrepel) #avoid label stacking when using ggplot
 library(viridisLite) #turbo 256 color scale
 library(patchwork) #for table presentation
 library(viridis)
+
+plot_mixture = function(df_location){
+  temp = read_xlsx(df_location) %>% 
+    mutate(log_intensity = log10(sum_intensity))
+  plot_ly(temp, 
+          x = ~monoisotopic_mass, 
+          y = ~apex_rt, 
+          z = ~log_intensity, 
+          color = ~sgRNA, 
+          type = "scatter3d", 
+          mode = "markers", 
+          marker = list(size = 5)) %>%
+    layout(scene = list(
+      xaxis = list(title = "Monoisotopic Mass (Da)"),
+      yaxis = list(title = "Time (min)"),
+      zaxis = list(title = "Log Intensity")
+    ))
+}
